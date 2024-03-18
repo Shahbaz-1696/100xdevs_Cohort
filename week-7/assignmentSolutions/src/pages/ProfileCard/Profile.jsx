159a650c043f3e529cf0f691c3df982967cb2910
Shahbaz-1696 <ansarishahbaz96@gmail.com>
2024-03-18 16:33:56 +0530

    Completed solution for question-1

--- week-7/assignmentSolutions/src/pages/ProfileCard/Profile.jsx
+++ week-7/assignmentSolutions/src/pages/ProfileCard/Profile.jsx
@@ -1,8 +1,44 @@
-
+import classes from "./Profile.module.scss";
 
 const Profile = () => {
+  const user =  {
+      name: "Shahbaz Ansari",
+      age: 27,
+      location: "India",
+      followers: "200K",
+      likes: "350K",
+      photos: 380,
+    }
+  
   return (
-    <div>Profile</div>
+    <div className={classes.main}>
+      <div className={classes.cardTop}></div>
+      <div className={classes.image}>
+        <img src="../../../public/profile_pic.png" alt="Image" width={80} height={80} />
+      </div>
+      <div className={classes.info}>
+        <h2 className={classes.name}>{user.name}</h2>
+        <p>{user.age}</p>
+      </div>
+      <div className={classes.location}>
+        <p>{user.location}</p>
+      </div>
+      <div className={classes.breakLine}></div>
+      <div className={classes.details}>
+        <div className={classes.followers}>
+          <h3 style={{padding: "0 15px"}}>{user.followers}</h3>
+          <p style={{color: "grey"}}>Followers</p>
+        </div>
+        <div className={classes.likes}>
+          <h3>{user.likes}</h3>
+          <p style={{color: "grey"}}>Likes</p>
+        </div>
+        <div className={classes.photos}>
+          <h3 style={{padding: "0 5px"}}>{user.photos}</h3>
+          <p style={{color: "grey"}}>Photos</p>
+        </div>
+      </div>
+    </div>
   )
 }
 
