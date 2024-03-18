6c977fd53c8955f20f3f6688cfafc02953dbadfe
Shahbaz-1696 <ansarishahbaz96@gmail.com>
2024-03-19 00:35:57 +0530

    Completed solution for question-4

--- week-7/assignmentSolutions/src/pages/ParaGenerator/ParaGenerator.jsx
+++ week-7/assignmentSolutions/src/pages/ParaGenerator/ParaGenerator.jsx
@@ -1,8 +1,70 @@
-
+import classes from "./ParaGenerator.module.scss";
+import { useMemo, useState, useRef } from "react";
 
 const ParaGenerator = () => {
+  const [wordCount, setWordCount] = useState(0);
+  let [paragraph, setParagraph] = useState("");
+  const inputRef = useRef();
+
+  const randomWords = [
+    "apple",
+    "banana",
+    "orange",
+    "grape",
+    "kiwi",
+    "elephant",
+    "giraffe",
+    "zebra",
+    "lion",
+    "tiger",
+    "mountain",
+    "river",
+    "ocean",
+    "forest",
+    "desert",
+    "programming",
+    "javascript",
+    "react",
+    "node",
+    "database",
+    "coffee",
+    "tea",
+    "pizza",
+    "burger",
+    "pasta",
+    "happy",
+    "sad",
+    "excited",
+    "relaxed",
+    "energetic",
+  ];
+
+  const generate = useMemo(() => {
+    const num = wordCount;
+    let temp = "";
+    for(let i = 0; i < num; i++){
+      temp += randomWords[Math.floor(Math.random() * randomWords.length)] + " ";
+    }
+    setParagraph(() => temp);
+  }, [wordCount]);
+
   return (
-    <div>ParaGenerator</div>
+    <div className={classes.main}>
+      <div className={classes.title}>
+        <h1>Para Generator</h1>
+      </div>
+      <div className={classes.input}>
+        <input type="text" 
+        placeholder="Enter Number of Words"
+        ref={inputRef} />
+        <button onClick={() => setWordCount(() => {
+          return Number(inputRef.current.value);
+        })}>Generate</button>
+      </div>
+      <div className={classes.result}>
+        <p>{paragraph}</p>
+      </div>
+    </div>
   )
 }
 
--- week-7/assignmentSolutions/src/pages/index.js
+++ week-7/assignmentSolutions/src/pages/index.js
@@ -1,7 +1,6 @@
 export { default as Home} from "./Home/Home";
 export { default as Profile } from "./ProfileCard/Profile";
 export { default as ColorChanger } from "./ColorChanger/ColorChanger";
-export { default as CustomReact } from "./CustomReact/CustomReact";
 export { default as ParaGenerator } from "./ParaGenerator/ParaGenerator.jsx";
 export { default as GithubCard } from "./GithubCard/GithubCard.jsx";
 export { default as Login } from "./LoginUsingOTP/Login.jsx";
