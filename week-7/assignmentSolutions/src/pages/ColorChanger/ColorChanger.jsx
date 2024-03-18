3afcc541d575ad0623062c5f2493c6b3bffe41ed
Shahbaz-1696 <ansarishahbaz96@gmail.com>
2024-03-18 17:13:23 +0530

    Completed solution for question-2

--- week-7/assignmentSolutions/src/pages/ColorChanger/ColorChanger.jsx
+++ week-7/assignmentSolutions/src/pages/ColorChanger/ColorChanger.jsx
@@ -1,8 +1,49 @@
-
+import classes from "./ColorChanger.module.scss";
+import { useEffect, useState } from "react";
 
 const ColorChanger = () => {
+  const [bgColor, setBgColor] = useState("");
+
+  const colors = [{
+    name: "Red",
+    color: "red",
+  }, {
+    name: "Yellow",
+    color: "yellow",
+  }, {
+    name: "Black",
+    color: "black",
+  }, {
+    name: "Purple",
+    color: "purple",
+  }, {
+    name: "Green",
+    color: "green",
+  }, {
+    name: "Blue",
+    color: "blue",
+  }, {
+    name: "Default",
+    color: "orange",
+  }]
+
+  useEffect(() => {
+    document.body.style.backgroundColor = bgColor
+  }, [bgColor]);
+
+  function handleOnClick (bgColor) {
+    console.log(bgColor);
+    setBgColor(bgColor);
+  }
+
   return (
-    <div>ColorChanger</div>
+    <div className={classes.main}>
+      {colors.map((color, index) => (
+        <div key={index}>
+          <button style={{backgroundColor: color.color}} className={classes.buttons} onClick={() => handleOnClick(color.color)}>{color.name}</button>
+        </div>
+      ))}
+    </div>
   )
 }
 
