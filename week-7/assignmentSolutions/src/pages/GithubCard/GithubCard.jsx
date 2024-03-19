f0090e65b1819ad1cb4949ba3399d8cd90bdece2
Shahbaz-1696 <ansarishahbaz96@gmail.com>
2024-03-19 12:44:22 +0530

    Completed solution for question 5

--- week-7/assignmentSolutions/src/pages/GithubCard/GithubCard.jsx
+++ week-7/assignmentSolutions/src/pages/GithubCard/GithubCard.jsx
@@ -1,9 +1,42 @@
-
+import classes from "./GithubCard.module.scss";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 const GithubCard = () => {
-  return (
-    <div>GithubCard</div>
-  )
+  const [res, setRes] = useState("");
+  const username = "Shahbaz-1696";
+  const api = `https://api.github.com/users/${username}`;
+
+  useEffect(() => {
+    axios.get(api).then((response) => setRes(() => response.data));
+  }, []);
+
+  if(res) {
+    return (
+      <div className={classes.main}>
+        <div className={classes.cardTop}></div>
+        <div>
+          <img src={res.avatar_url} alt="Image" width={80} height={80} className={classes.image} />
+        </div>
+        <div className={classes.info}>
+          <h1>{res.name}</h1>
+          <h3>{res.bio}</h3>
+        </div>
+        <div className={classes.break}></div>
+        <div className={classes.details}>
+          <h4>Location: {res.location}</h4>
+          <p>Followers: {res.followers}</p>
+          <p>Following: {res.following}</p>
+          <p>Public Repos: {res.public_repos}</p>
+          <p>Twitter: {res.twitter_username}</p>
+        </div>
+      </div>
+    )
+  } else {
+    return null
+  }
+
+  
 }
 
 export default GithubCard
